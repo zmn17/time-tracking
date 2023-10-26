657cc0141c0bd14770aff3b784b96282119cd69d
Zamin Ahmadi <asam79961@gmail.com>
2023-10-26 23:31:59 +1100

    finish the styling

---
+++ script.js
@@ -0,0 +1,9 @@
+/* 
+    1. create the nav for - period
+    2. create hover effects for the nav
+    3. effect: the one period chosen should be selected
+    4. use data json for filling the card information
+    5. create hover effect for the card
+    6. create hover effect for the dots icons
+    7. create the dots icon functionality
+*/
