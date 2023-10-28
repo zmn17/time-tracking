3ac22f9b3fd0fe0f149e306aa768606cbf2c64a4
Zamin Ahmadi <asam79961@gmail.com>
2023-10-28 22:21:47 +1100

    finish the style and the logic

--- script.js
+++ script.js
@@ -7,3 +7,88 @@
     6. create hover effect for the dots icons
     7. create the dots icon functionality
 */
+
+// Buttons
+const checkboxes = document.querySelectorAll('.box');
+const labelTime = document.querySelectorAll('.label');
+
+// titles : work, play, study, exercise, social and self care
+const title = document.querySelectorAll('.card__title');
+const current = document.querySelectorAll('.card__hours');
+const previous = document.querySelectorAll('.card__last');
+
+
+// checkbox selections behaviour
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function () {
+        // Uncheck all checkboxes
+        checkboxes.forEach((otherCheckbox) => {
+            if (otherCheckbox !== checkbox) {
+                otherCheckbox.checked = false;
+            }
+        });
+    });
+
+});
+
+
+// event listeners for checkboxes -> to change the color of the its label
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', () => {
+        checkedBox();
+        fillData();
+    });
+});
+
+    
+
+// callback for checkboxes -> change the color of the label
+const checkedBox = () => {
+   for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+        labelTime[i].style.color = '#fff'; // change the color of the label
+
+    } else {
+        labelTime[i].style.color = ''; // update the color of the label back to the default
+    }
+   }
+}
+
+
+
+
+// default setting for 'daily' label
+checkboxes[1].checked = true;
+labelTime[1].style.color = '#fff';
+
+// fetch the data.json
+const fetchData = async (dataset) => {
+    try {
+        const res = await fetch(dataset);
+        return res.json();
+    } catch (err) {
+        throw err;
+    }
+}
+
+// populating data for title
+const fillData = async () => {
+    const data = await fetchData('data.json');
+    for (let i = 0; i < data.length; i++) {
+        title[i].innerHTML = data[i]['title'];
+        if (checkboxes[0].checked) {
+            current[i].innerHTML = data[i]['timeframes']['daily']['current'] + 'hrs';
+            previous[i].innerHTML = 'Last Week - ' + data[i]['timeframes']['daily']['previous'] + 'hrs';
+        } else if (checkboxes[1].checked) {
+            current[i].innerHTML = data[i]['timeframes']['weekly']['current'] + 'hrs';
+            previous[i].innerHTML = 'Last Week - ' + data[i]['timeframes']['weekly']['previous'] + 'hrs'; 
+        } else if (checkboxes[2].checked) {
+            current[i].innerHTML = data[i]['timeframes']['monthly']['current'] + 'hrs';
+            previous[i].innerHTML = 'Last Week - ' + data[i]['timeframes']['monthly']['previous'] + 'hrs';
+        }
+    }       
+}
+fillData();
+
+
+
